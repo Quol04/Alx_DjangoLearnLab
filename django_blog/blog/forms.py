0b01029e04052677ce22bd0c0eeef0e6b603a778
quol <kuolamol77@gmail.com>
2025-12-07 22:43:37 +0300

    add comment model equals comment

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -35,7 +35,7 @@ class PostForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = __import__('blog.models', fromlist=['Comment']).Comment
+        model = Comment
         fields = ("content",)
         widgets = {
             "content": forms.Textarea(attrs={"rows": 4, "placeholder": "Write your comment..."}),
