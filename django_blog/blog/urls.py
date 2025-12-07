8a304a50648104fa8163c812a8289642b60fb6e0
quol <kuolamol77@gmail.com>
2025-12-07 22:46:14 +0300

    update  comment urls for update and adding new comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -30,8 +30,8 @@ urlpatterns += [
 
 # Comments
 urlpatterns += [
-    path('post/<int:post_pk>/comments/new/',
+    path('post/<int:pk>/comments/new/',
          CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
